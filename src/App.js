5e1dc9700eca73f351697e211f7560c4dc2c405f
Vishwas2019 <2019ucp1352@mnit.ac.in>
2021-06-25 18:47:15 +0530

    vishwas's commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn from a professional animator vishwas agrawal.
         </a>
       </header>
     </div>
