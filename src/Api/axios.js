eb7a9f69986df149494b7be324a333e85d2d2b77
kunallverma007 <58910303+kunallverma007@users.noreply.github.com>
2021-06-27 22:12:34 +0530

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 const Axios=axios.create({
-	baseURL:'https://ezymailserver.herokuapp.com/'
+	baseURL:'https://ezymailserver.herokuapp.com/',
+	headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    }
 });
 export default Axios;
